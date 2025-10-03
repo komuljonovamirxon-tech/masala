daff8e9b3714dd723429452468e95bd4719fc626
Amirxon <komuljonovamirxon@gmail.com>
2025-10-03 19:06:43 +0500

    Add files via upload

---
+++ masala20.py
@@ -0,0 +1,112 @@
+# #1-masala
+# a = int(input('son1:'))
+# b = int(input('son2:'))
+# summa = a + b
+# print(summa)
+# #2-masala
+# c = int(input('eni:'))
+# d = int(input("bo'yi:"))
+# area = c * d
+# print(area)
+# #3-masala
+# e = int(input('son1:'))
+# f = int(input("son2:"))
+# j = int(input("son3:"))
+# avg = (e+f+j)/3
+# print(avg)
+# h = int(input('t1:'))
+# i = int(input('t2:'))
+# pe = (h+i)*2
+# print(pe)
+#5 - masala
+# k = int(input("soat:"))
+# l = int(input("daqiqa:"))
+# total_min = k * 60 + l
+# print(total_min)
+# #6-masala
+# usd = int(input("usd:"))
+# summ = usd * 12500
+# print(summ)
+#7-masala
+# u = int(input("h1:"))
+# r = int(input("h2:"))
+# diff = abs(u - r)
+# print(diff)
+# #8-masala
+# x = int(input("asos:"))
+# n = int(input("daraja:"))
+# power = x ** n
+# print(power)
+# #9-masala
+# f = int(input("son2:"))
+# g = int(input("son2:"))
+# quo = f//g
+# print(quo)
+# #10-masala
+# q = int(input("son2:"))
+# w = int(input("son2:"))
+# h = q,w = w,q
+# print(h)
+# #11-masala
+# n = int(input("ijara xaqqi:"))
+# rent = 200000
+# per_month = rent / n
+# print(per_month)
+# #12-masala
+# t = int(input("km/h:"))
+# m = 200
+# time = m / t
+# print(time,"soat")
+# #13-masala
+# odam = int(input("odam soni:"))
+# pessenger = int(input("chipta:"))
+# seats = 16
+# if odam > seats:
+#     print("joy yo'q")
+# elif pessenger > seats:
+#     print("chiptalar ortiqcha")
+# elif pessenger < odam:
+#     print("chipta yetmadi:")
+# left = pessenger % seats
+# print("qolgan joy",left)
+# #14-masala
+# litr = int(input("yoqilg'i:"))
+# bosilgan_masofa = int(input("masofa:"))
+# fuel = litr * bosilgan_masofa / 100
+# print(fuel)
+# #15-masala
+# salary = int(input("maosh:"))
+# net = salary * (1-0.12)
+# print(net)
+# #16-masala
+# am = int(input("so'm:"))
+# days = int(input("kechikkan:"))
+# penalty = am * 0.001 * days
+# total = am - penalty
+# print(total)
+#17-masala
+# price = int(input("mahsulot narxi:"))
+# discount = int(input("sotib oluvchi:"))
+# paid = price * (1 - discount/100)
+# print(paid)
+# #18-masala
+# p = int(input("asos:"))
+# r = int(input("yillik foiz:"))
+# t = int(input("yillar:"))
+# interest = p * r / 100 * t
+# total = p + interest
+# print(total)
+# #19-masala
+# base = int(input("retsept:"))
+# people = int(input("odam soni:"))
+# per_person = base/4
+# needed = per_person * people
+# print(needed)
+# #20-masala
+# bill = int(input("hisob-kitob:"))
+# tip = bill*0.10
+# total = bill + tip
+# print(total)
+
+
+
